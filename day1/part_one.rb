ff718436ab9ce9b39f74ca5409cdf34af54faee4
Phillip Davis <phillipdavis@Phillips-MacBook-Pro.local>
2022-07-27 12:33:14 -0400

    Remove DS_Store, add it to .gitignore, and fix type issue with part_one

--- day1/part_one.rb
+++ day1/part_one.rb
@@ -5,6 +5,6 @@ num_increases = 0
 for i in 1...input_split.size do
     first = input_split[i - 1]
     second = input_split[i] 
-    num_increases += second > first ? 1 : 0
+    num_increases += second.to_i > first.to_i ? 1 : 0
 end
 puts num_increases
